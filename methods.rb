c4997878730c2821d2ab17ad98384d492e43f212
Nadia Virjee <nadvirjee@gmail.com>
2023-01-27 15:12:57 -0500

    commit tree 79257087977424f5188a580c596fd6842dcaeaf2

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
-# Your code here!
+#test-1
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+#test-2
+
+def greet name 
+    puts "Hello, #{name}!"
+end
+
+#test-3
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+
+#test-4
+
+def add num1, num2
+    num1 + num2
+end
+
+#test-5
+
+def halve number
+    if number.class != number
+        return nil
+    end
+
+    number/2
+end
